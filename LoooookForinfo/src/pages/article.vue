c20284fb8aad6f0a0e22067e7ff8f7e1421d4b30
CNlitai <2083645366@qq.com>
2020-09-01 15:32:46 +0800

    Create article.vue

---
+++ LoooookForinfo/src/pages/article.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    文章管理
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods() {
+    axios({
+      url: "http://139.196.24.143:8099/"
+      
+    })
+  }
+}
+</script>
